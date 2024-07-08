1f6c0d963feede3c9e6449b4e76f887d4106d9f9
jocarsa <info@josevicentecarratala.com>
2024-07-08 11:00:52 +0200

    7

--- gimpreact/src/Bloques/Secciones/Herramientas/Herramienta.js
+++ gimpreact/src/Bloques/Secciones/Herramientas/Herramienta.js
@@ -1,10 +1,13 @@
 import './Herramienta.css';
 function Herramienta() {
-  return ( 
-        <button>
-            F  
+    function hazClick(){
+        alert("Has hecho click")
+    }
+    return ( 
+        <button onClick={hazClick}>
+            F 
         </button>    
-  );
+    );
 }
 
 export default Herramienta;
