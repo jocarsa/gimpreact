673d73811601fa19a6e4f0fe8c19c37cdee489b1
jocarsa <info@josevicentecarratala.com>
2024-07-08 11:09:56 +0200

    9

--- gimpreact/src/Bloques/Secciones/Herramientas/Herramienta.js
+++ gimpreact/src/Bloques/Secciones/Herramientas/Herramienta.js
@@ -1,19 +1,25 @@
 import './Herramienta.css';
+
+function mover(){
+    alert("Has hecho click en mover")
+}
+function seleccion(){
+    alert("Has hecho click en seleccion")
+}
+function lazo(){
+    alert("Has hecho click en lazo")
+}
+function varita(){
+    alert("Has hecho click en varita")
+}
+
 function Herramienta({accion,children}) {
     function hazClick(){
         switch(accion){
-            case "Mover":
-                alert("Has hecho click en mover")
-                break;
-            case "Seleccion":
-                alert("Seleccion")
-                break;
-            case "Lazo":
-                alert("Has hecho click en lazo")
-                break;
-            case "Varita":
-                alert("Has hecho click en varita")
-                break;
+            case "Mover":       mover();break;
+            case "Seleccion":   seleccion();break;
+            case "Lazo":        lazo();break;
+            case "Varita":      varita();break;
         }
     }
     return ( 
